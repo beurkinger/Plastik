614ca681b0ccc79bc71ddf01b1128bd61589fc10
beurkinger <tgoehringer@gmail.com>
2017-06-09 15:59:55 +0200

    menu updated

--- menu.php
+++ menu.php
@@ -5,33 +5,33 @@ $page = isset($page) ? $page : '';
   <h1>Plastik</h1>
   <ul>
     <li>
-        <a href="index.php">
+        <a href="index.php" class="inactive">
           A<span>ccuei</span>l
         </a>
     </li>
     <li>
-      <a href="search.php">
+      <a href="search.php" class="inactive">
         R<span>echerch</span>e
       </a>
     </li>
     <li>
-      <a href="magazines.php">
+      <a href="magazines.php" class="active">
         N<span>uméro</span>s
       </a>
       <?php if ($page === "article") require('./menuArticle.php'); ?>
     </li>
     <li>
-      <a href="authors.php">
+      <a href="authors.php" class="inactive">
         A<span>uteur</span>s
       </a>
     </li>
     <li>
-      <a href="contribute.php">
+      <a href="contribute.php" class="inactive">
         C<span>ontribue</span>r
       </a>
     </li>
     <li>
-      <a href="contact.php">
+      <a href="contact.php" class="inactive">
         C<span>ontac</span>t
       </a>
     </li>
